b0889b3492b8463766438cbe634a5829a85e9e0b
OmelianLevkovych <oml@e-boks.dk>
2021-06-26 11:24:53 +0300

    (draft) Initial commit

---
+++ src/hackaton-aws/hackaton-aws.tests/SimpleTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace hackaton_aws.tests
+{
+    public class SimpleTest
+    {
+        [Fact]
+        public void TestExample()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
